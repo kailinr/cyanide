ab517d10e40fcfb1757c9d758b5dd4585586cde9
kailin <kailin@torvadata.com>
2022-06-10 14:29:21 -0700

    edit IERC20 comment

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -11,6 +11,7 @@ interface IERC20 {
         function name() external view returns (string memory);
         function symbol() external view returns (string memory);
         function decimals() external view returns (uint8);
+        //
         function transferFrom(
             address from,
             address to,
