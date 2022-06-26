544381fda6a214c3e54396c2ecee712e2576ddc3
kailin <kailin@torvadata.com>
2022-06-25 18:11:12 -0700

    removed standardtoken test

--- test/test.js
+++ test/test.js
@@ -2,21 +2,6 @@ const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 const utils = ethers.utils;
 
-describe("------- Standard Token ----------  ", function () {
-  let token, deployer;
-
-  before(async () => {
-    [deployer, user] = await ethers.getSigners();
-  });
-
-  it("should mint token", async () => {
-    const Token = await hre.ethers.getContractFactory("StandardToken");
-    token = await Token.deploy();
-    await token.deployed();
-    console.log("Deployed Token to:", token.address);
-  });
-});
-
 describe("------- BackdoorToken ----------  ", function () {
   let token, deployer, user;
   before(async () => {
